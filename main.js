5a9d7fc5f82d5b03102c23abbe406587d6b9e9e0
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-11-13 11:18:37 +0700

    update

--- main.js
+++ main.js
@@ -2,10 +2,25 @@ import { argv } from "process";
 import fs from "node:fs";
 import readline from "readline";
 import events from "events";
-import { firstPass, secondPass } from "./src/assembly";
-import { fileResolver } from "./src/IOFileResolver";
+import { firstPass, secondPass } from "./src/assembly.js";
+import { fileResolver } from "./src/IOFileResolver.js";
 
-async function main(file, out) {
+(async function main() {
+  if (argv[2] === undefined) {
+    showHelp();
+    return;
+  }
+  assembler(argv[2], argv[3]);
+})();
+
+function showHelp() {
+  console.log(`Hack Assembler
+USAGE
+  hasm <source> <output>
+`);
+}
+
+async function assembler(file, out) {
   let [input, output] = fileResolver(file, out);
   const outStream = fs.createWriteStream(output);
   const firstRL = readline.createInterface({
@@ -26,5 +41,3 @@ async function main(file, out) {
     outStream.write(secondPass(line));
   });
 }
-
-main(argv[2], argv[3]);
